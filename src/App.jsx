ae084a0dec05e806f715ab06ad14eb234412120b
Amin NAIRI <nairi_a16@myges.fr>
2020-07-13 00:22:28 +0200

    :sparkles: merchant new page, user context & miscellaneous

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,53 @@
 import React from "react";
 import HomePage from "./pages/Home";
-import NotFoundPage from "./pages/NotFound";
+import NotFoundConnectedPage from "./pages/NotFoundConnected";
+import NotFoundDisconnectedPage from "./pages/NotFoundDisconnected";
 import DashboardPage from "./pages/Dashboard";
+import MerchantNewPage from "./pages/MerchantNew";
 import MerchantsPage from "./pages/Merchants";
-import MerchantPage from "./pages/Merchant";
 import TransactionsPage from "./pages/Transactions";
 import SigninPage from "./pages/Signin";
 import SignupPage from "./pages/Signup";
 import Header from "./components/Header";
 import Menu from "./components/Menu";
-import {BrowserRouter, Route, Switch, Redirect} from "react-router-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import {MenuProvider} from "./contexts/Menu";
+import {UserProvider, useUserContext} from "./contexts/User";
+
+const Routes = () => {
+    const {user} = useUserContext();
+
+    if (user.token) {
+        return <Switch>
+            <Route path="/" component={HomePage} exact />
+            <Route path="/dashboard" component={DashboardPage} exact />
+            <Route path="/merchants" component={MerchantsPage} exact />
+            <Route path="/merchant/new" component={MerchantNewPage} exact />
+            <Route path="/transactions" component={TransactionsPage} exact />
+            <Route path="*" component={NotFoundConnectedPage} />
+        </Switch>;
+    }
+
+    return <Switch>
+        <Route path="/signin" component={SigninPage} exact />
+        <Route path="/signup" component={SignupPage} exact />
+        <Route path="*" component={NotFoundDisconnectedPage} />
+    </Switch>;
+};
 
 const App = () => {
-    const userConnected = false;
-
     return (
         <React.StrictMode>
-            <BrowserRouter>
-                <MenuProvider>
-                    <Menu />
-                    <Header />
-                </MenuProvider>
+            <UserProvider>
+                <BrowserRouter>
+                    <MenuProvider>
+                        <Menu />
+                        <Header />
+                    </MenuProvider>
 
-                <Switch>
-                    {userConnected ? (
-                        <>
-                            <Route path="/" component={HomePage} exact />
-                            <Route path="/dashboard" component={DashboardPage} exact />
-                            <Route path="/merchants" component={MerchantsPage} exact />
-                            <Route path="/merchants/:id" component={MerchantPage} exact />
-                            <Route path="/transactions" component={TransactionsPage} exact />
-                            <Route component={NotFoundPage} />
-                        </>
-                    ) : (
-                        <>
-                            <Route path="/signin" component={SigninPage} exact />
-                            <Route path="/signup" component={SignupPage} exact />
-                            <Redirect to="/signin" />
-                        </>
-                    )}
-                </Switch>
-            </BrowserRouter>
+                    <Routes />
+                </BrowserRouter>
+            </UserProvider>
         </React.StrictMode>
     );
 };
