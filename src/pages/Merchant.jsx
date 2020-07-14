bd889ca8d9e3747f0ba1c4ce5f32d4ee9fe3605a
Amin NAIRI <nairi_a16@myges.fr>
2020-07-14 13:17:14 +0200

    :sparkles: added the details view

--- src/pages/Merchant.jsx
+++ src/pages/Merchant.jsx
@@ -1,124 +1,229 @@
-import React, {useState} from "react";
+import React, {useEffect, useState, useCallback} from "react";
 import {useParams} from "react-router-dom";
-import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
-import Grid from "@material-ui/core/Grid";
 import {useAuthenticatedRequest} from "../hooks/request";
 import Snackbar from "@material-ui/core/Snackbar";
-import TextField from "@material-ui/core/TextField";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
 import {makeStyles} from "@material-ui/core/styles";
-import {useFormState} from "../hooks/form";
+import TableContainer from "@material-ui/core/TableContainer";
+import Paper from "@material-ui/core/Paper";
+import Table from "@material-ui/core/Table";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import TableBody from "@material-ui/core/TableBody";
+import {Link} from "react-router-dom";
 import Button from "@material-ui/core/Button";
+import TableHead from "@material-ui/core/TableHead";
 
 const useStyles = makeStyles(() => ({
     grid: {
         marginTop: "20px"
-    },
-    textField: {
-        width: "100%"
     }
 }));
 
 const Merchant = () => {
-    const styles = useStyles();
     const {id} = useParams();
-    const {postRequest} = useAuthenticatedRequest();
+    const {getRequest, putRequest, deleteRequest} = useAuthenticatedRequest();
+    const styles = useStyles();
     const [snackbarOpen, setSnackbarOpen] = useState(false);
     const [snackbarMessage, setSnackbarMessage] = useState("");
-    const [name, setFormName] = useFormState("");
-    const [kbisUrl, setFormKbisUrl] = useFormState("");
-    const [currency, setFormCurrency] = useFormState("");
-    const [contactName, setFormContactName] = useFormState("");
-    const [email, setFormEmail] = useFormState("");
-    const [phoneNumber, setFormPhoneNumber] = useFormState("");
+    const [merchant, setMerchant] = useState(null);
 
-    const update = () => {
-        postRequest(`merchants/${id}`, {
-            name,
-            KBISUrl: kbisUrl,
-            currency,
-            contactName,
-            email,
-            phoneNumber
-        }).then(() => {
-            setSnackbarMessage("Successfully updated the merchant details");
-            setSnackbarOpen(true);
+    useEffect(() => {
+        setSnackbarMessage("Retrieving data...");
+        setSnackbarOpen(true);
+
+        getRequest(`merchants/${id}`).then(json => {
+            setMerchant(json);
+            setSnackbarOpen(false);
+            setSnackbarMessage("");
         }).catch(() => {
-            setSnackbarMessage("Failed to update the merchant details");
+            setSnackbarMessage("Failed to retrieve details for the merchant");
             setSnackbarOpen(true);
         });
-    };
+    }, []);
+
+    const validate = useCallback(() => {
+        putRequest(`merchants/${id}/validate`).then((json) => {
+            setMerchant(json);
+            setSnackbarMessage("Successfully validated the merchant");
+        }).catch(() => {
+            setSnackbarMessage("Failed to validate the merchant");
+        }).finally(() => {
+            setSnackbarOpen(true);
+        }); 
+    });
+
+    const revoke = useCallback(() => {
+        deleteRequest(`merchants/${id}/credentials`).then((json) => {
+            setMerchant(json);
+            setSnackbarMessage("Successfully revoked credentials");
+        }).catch(() => {
+            setSnackbarMessage("Failed to revoke credentials");
+        }).finally(() => {
+            setSnackbarOpen(true);
+        });
+    });
+
+    const regenerate = useCallback(() => {
+        getRequest(`merchants/${id}/credentials`).then((json) => {
+            setMerchant(json);
+            setSnackbarMessage("Successfully regenerated credentials");
+        }).catch(() => {
+            setSnackbarMessage("Failed to regenerate the credentials");
+        }).finally(() => {
+            setSnackbarOpen(true);
+        });
+    });
+
+    if (!merchant) {
+        return (
+            <Container>
+                <Typography variant="h3">Merchant #{id}</Typography>
+            </Container>
+        );
+    }
 
     return (
         <Container>
+            <Grid justify="center" className={styles.grid} container>
+                <Typography variant="h3">
+                    <span>Merchant #{id} </span>
+                    <span>&nbsp;</span>
+                    <Link to={`/merchant/${id}/transactions`}>
+                        <Button variant="contained" color="primary">Transactions</Button>
+                    </Link>
+                </Typography>
+                <Snackbar
+                    anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+                    open={snackbarOpen}
+                    autoHideDuration={6000}
+                    onClose={() => setSnackbarOpen(false)}
+                    message={snackbarMessage} />
+                <TableContainer component={Paper}>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Designation</TableCell>
+                                <TableCell>Value</TableCell>
+                                <TableCell>Action</TableCell>
+                            </TableRow>
+                        </TableHead>
 
-            <Snackbar
-                anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
-                open={snackbarOpen}
-                autoHideDuration={6000}
-                onClose={() => setSnackbarOpen(false)}
-                message={snackbarMessage} />
+                        <TableBody>
+                            <TableRow>
+                                <TableCell>Identifier</TableCell>
+                                <TableCell>{merchant.id}</TableCell>
+                                <TableCell></TableCell>
+                            </TableRow>
 
-            <Grid justify="center" className={styles.grid} spacing={3} container>
-                <Grid xs={12} sm={8} lg={6} xl={6} justify="center" spacing={3} item container>
-                    <Typography variant="h3">New Merchant</Typography>
+                            <TableRow>
+                                <TableCell>Name</TableCell>
+                                <TableCell>{merchant.name}</TableCell>
+                                <TableCell></TableCell>
+                            </TableRow>
 
-                    <Grid xs={12} item>
-                        <TextField
-                            className={styles.textField}
-                            variant="outlined"
-                            label="Name"
-                            value={name}
-                            onInput={setFormName} />
-                    </Grid>
+                            <TableRow>
+                                <TableCell>KBis URL</TableCell>
+                                <TableCell>{merchant.KBISUrl}</TableCell>
+                                <TableCell></TableCell>
+                            </TableRow>
 
-                    <Grid xs={12} item>
-                        <TextField
-                            className={styles.textField}
-                            variant="outlined"
-                            label="KBis URL"
-                            value={kbisUrl}
-                            onInput={setFormKbisUrl} />
-                    </Grid>
+                            <TableRow>
+                                <TableCell>Currency</TableCell>
+                                <TableCell>{merchant.currency}</TableCell>
+                                <TableCell></TableCell>
+                            </TableRow>
 
-                    <Grid xs={12} item>
-                        <TextField
-                            className={styles.textField}
-                            variant="outlined"
-                            label="Currency"
-                            value={currency}
-                            onInput={setFormCurrency} />
-                    </Grid>
+                            <TableRow>
+                                <TableCell>Contact Name</TableCell>
+                                <TableCell>{merchant.contactName}</TableCell>
+                                <TableCell></TableCell>
+                            </TableRow>
 
-                    <Grid xs={12} item>
-                        <TextField
-                            className={styles.textField}
-                            variant="outlined"
-                            label="Contact Name"
-                            value={contactName}
-                            onInput={setFormContactName} />
-                    </Grid>
+                            <TableRow>
+                                <TableCell>Email</TableCell>
+                                <TableCell>{merchant.email}</TableCell>
+                                <TableCell></TableCell>
+                            </TableRow>
 
-                    <Grid xs={12} item>
-                        <TextField
-                            className={styles.textField}
-                            variant="outlined"
-                            label="Email"
-                            value={email}
-                            onInput={setFormEmail} />
-                    </Grid>
+                            <TableRow>
+                                <TableCell>Phone Number</TableCell>
+                                <TableCell>{merchant.phoneNumber}</TableCell>
+                                <TableCell></TableCell>
+                            </TableRow>
 
-                    <Grid xs={12} item>
-                        <TextField
-                            className={styles.textField}
-                            variant="outlined"
-                            label="Phone Number"
-                            value={phoneNumber}
-                            onInput={setFormPhoneNumber} />
-                    </Grid>
+                            <TableRow>
+                                <TableCell>Confirm URL</TableCell>
+                                <TableCell>{merchant.confirmUrl}</TableCell>
+                                <TableCell></TableCell>
+                            </TableRow>
 
-                    <Button variant="contained" color="primary" onClick={update}>Update</Button>
-                </Grid>
+                            <TableRow>
+                                <TableCell>Cancel URL</TableCell>
+                                <TableCell>{merchant.cancelUrl}</TableCell>
+                                <TableCell></TableCell>
+                            </TableRow>
+
+                            <TableRow>
+                                <TableCell>Status</TableCell>
+                                <TableCell>{merchant.status}</TableCell>
+                                <TableCell>
+                                    {(merchant.status !== "validated" || !merchant.token || !merchant.secret) && <Button variant="contained" color="primary" onClick={validate}>Validate</Button>}
+                                </TableCell>
+                            </TableRow>
+
+                            <TableRow>
+                                <TableCell>Credits</TableCell>
+                                <TableCell>{merchant.credit}</TableCell>
+                                <TableCell></TableCell>
+                            </TableRow>
+
+                            <TableRow>
+                                <TableCell>Token</TableCell>
+                                <TableCell>{merchant.token}</TableCell>
+                                <TableCell>
+                                    <Button variant="contained" color="primary" onClick={revoke}>Revoke</Button>
+                                    <span>&nbsp;</span>
+                                    <Button variant="contained" color="primary" onClick={regenerate}>Regenerate</Button>
+                                </TableCell>
+                            </TableRow>
+
+                            <TableRow>
+                                <TableCell>Secret</TableCell>
+                                <TableCell>{merchant.secret}</TableCell>
+                                <TableCell>
+                                    <Button variant="contained" color="primary" onClick={revoke}>Revoke</Button>
+                                    <span>&nbsp;</span>
+                                    <Button variant="contained" color="primary" onClick={regenerate}>Regenerate</Button>
+                                </TableCell>
+                            </TableRow>
+
+                            <TableRow>
+                                <TableCell>Created At</TableCell>
+                                <TableCell>{merchant.createdAt}</TableCell>
+                                <TableCell></TableCell>
+                            </TableRow>
+
+                            <TableRow>
+                                <TableCell>Updated At</TableCell>
+                                <TableCell>{merchant.updatedAt}</TableCell>
+                                <TableCell></TableCell>
+                            </TableRow>
+
+                            <TableRow>
+                                <TableCell>User</TableCell>
+                                <TableCell></TableCell>
+                                <TableCell>
+                                    <Link to={`/user/${id}`}>
+                                        <Button color="primary" variant="contained">See</Button>
+                                    </Link>
+                                </TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </TableContainer>
             </Grid>
         </Container>
     );
